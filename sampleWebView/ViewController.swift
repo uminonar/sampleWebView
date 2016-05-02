b44b168c84e991dd077b9fb6a1cbbb56d00c9c09
Riho Okubo <rihoohkubo1002@gmail.com>
2016-05-02 18:27:16 +0800

    commit init

--- sampleWebView/ViewController.swift
+++ sampleWebView/ViewController.swift
@@ -10,10 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myWebView: UIWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 表示したいページのURL
+        // http://www.yahoo.co.jp/
+        
+        //URL型に代入
+        //var myURL = NSURL(string:"http://www.yahoo.co.jp/")
+        
+        //webViewに指定するため、NSURLRequest型のオブジェクトを作成
+        //var myURLReq = NSURLRequest(URL: myURL!)
+        
+        //webViewにセット
+        //myWebView.loadRequest(myURLReq)
+        
+        //TODO:テキストフィイールドを一つ設置し、そこに入力されたURLのページを表示するようにカスタマイズしましょう（Returnを押したら、ページを表示するように変更する）
+        
+        
+        
     }
+    
+        
+    @IBAction func tapReturn(sender: UITextField) {
+        var myURL = NSURL(string: sender.text!)
+        print(sender.text)
+        var myURLReq = NSURLRequest(URL: myURL!)
+        
+        myWebView.loadRequest(myURLReq)
+        
+    }
+   
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
